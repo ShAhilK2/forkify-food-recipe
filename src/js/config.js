fa95b0eb7b1bb8887474d193f4bc091f10ffeb03
Shahil Kataria <shahilkataria@Shahils-MacBook-Air.local>
2024-09-02 15:15:59 +0530

    Initial Commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '0b76af6e-a7a2-41b9-8060-47aa6b2db7e7';
+export const MODAL_CLOSE_SEC = 2.5;
